5ff882edbfb7ce641c687d586fbcef2dfce761df
CyberSenwin <cybersenwin@proton.me>
2024-06-14 02:02:28 -0700

    Add files via upload

---
+++ write_read_file.py
@@ -0,0 +1,7 @@
+#opening the input file in write mode
+input_file = open('the_file.txt', 'w')
+
+#write content to the plain text 
+input_file.write('Reading and writing file to input file folder.\n')
+input_file.write('Checking the file cause is been along time i did this in python. \n')
+
